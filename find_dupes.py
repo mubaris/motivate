bb0fc90f252293ac3b8d101f3b6c723e401f98b8
Serban Constantin <serban.constantin@sparkware.ro>
2017-10-17 12:28:08 +0300

    pep8 fixes

--- find_dupes.py
+++ find_dupes.py
@@ -8,16 +8,16 @@ data_dir = os.path.join(scriptpath, 'data')
 all_json = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
 quotes = []
 for f in all_json:
-	filename = os.path.join(data_dir, f)
-	with open(filename) as json_data:
-		quotes += json.load(json_data)['data']
-		
-uniq_authors = { quote['author'] for quote in quotes}
-uniq_quotes = { quote['quote'] for quote in quotes}
+    filename = os.path.join(data_dir, f)
+    with open(filename) as json_data:
+        quotes += json.load(json_data)['data']
 
-print('Unique quotes: {}, authors: {}'.format(len(uniq_quotes), len(uniq_authors))) 
+uniq_authors = {quote['author'] for quote in quotes}
+uniq_quotes = {quote['quote'] for quote in quotes}
+
+print('Unique quotes: {}, authors: {}'.format(len(uniq_quotes), len(uniq_authors)))
 
 seen = set()
-dupes = sorted([x for x in quotes if x['quote'] in seen or seen.add(x['quote'])], key=lambda x:x['quote'])
+dupes = sorted([x for x in quotes if x['quote'] in seen or seen.add(x['quote'])], key=lambda x: x['quote'])
 
 print(*dupes, sep='\n')
--- unique_quotes.py
+++ unique_quotes.py
@@ -8,22 +8,22 @@ data_dir = os.path.join(scriptpath, 'data')
 all_json = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
 quotes = []
 for f in all_json:
-	filename = os.path.join(data_dir, f)
-	with open(filename) as json_data:
-		quotes += json.load(json_data)['data']
+    filename = os.path.join(data_dir, f)
+    with open(filename) as json_data:
+        quotes += json.load(json_data)['data']
 
 unique_quotes = []
 seen_quotes = set()
 for x in quotes:
-	if(x['quote'] not in seen_quotes):
-		unique_quotes += [x]
-		seen_quotes.add(x['quote'])
-		
-with open('data_unique/unique_quotes.json', 'w') as outfile:
-	outfile.write(u'{ \"data\": [')
-	for i in range(len(unique_quotes)):
-		if(i != 0):
-			outfile.write(u',')
+    if x['quote'] not in seen_quotes:
+        unique_quotes += [x]
+        seen_quotes.add(x['quote'])
 
-		json.dump(unique_quotes[i], outfile, ensure_ascii=False, indent=4)
-	outfile.write(u']}')
+with open('data_unique/unique_quotes.json', 'w') as outfile:
+    outfile.write(u'{ \"data\": [')
+    for i in range(len(unique_quotes)):
+        if i != 0:
+            outfile.write(u',')
+
+        json.dump(unique_quotes[i], outfile, ensure_ascii=False, indent=4)
+    outfile.write(u']}')
