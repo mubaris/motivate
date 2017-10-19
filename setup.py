bb487209ff772aafa96c79497efd7f97ca8799be
Mubaris NK <mubarishassannk@gmail.com>
2017-10-19 09:41:37 +0530

    Ready for PyPi

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='motivate',
-    version='0.1',
+    version='0.2',
     description='A simple script to print random motivational quotes.',
     url='https://github.com/mubaris/motivate',
+    download_url='https://github.com/mubaris/motivate/archive/0.1.tar.gz',
     author='mubaris',
     author_email='mubarishassannk@gmail.com',
     license='MIT',
-    packages=['motivate'],
+    keywords = ['motivation', 'quotes'],
+    packages=find_packages(),
     zip_safe=False
     )
