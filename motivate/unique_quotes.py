2efd43ec8514fa0c5062ede4dc622d53ee628ecc
Jon <17364827+sesh22@users.noreply.github.com>
2017-10-31 21:10:30 +0000

    Refactored unique_quotes.py, and regenerated unique_quotes.json with it.

--- motivate/unique_quotes.py
+++ motivate/unique_quotes.py
@@ -5,24 +5,19 @@ import os
 
 scriptpath = os.path.dirname(__file__)
 data_dir = os.path.join(scriptpath, 'data')
-all_json = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
+quote_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
 quotes = []
-for f in all_json:
-    filename = os.path.join(data_dir, f)
-    with open(filename) as json_data:
-        quotes += json.load(json_data)['data']
+for f in quote_files:
+    with open(f, 'r') as quote_file:
+        quotes += json.load(quote_file)['data']
 
 unique_quotes = []
 seen_quotes = set()
 for x in quotes:
-    if(x['quote'] not in seen_quotes):
-        unique_quotes += [x]
+    if x['quote'] not in seen_quotes:
+        unique_quotes.append(x)
         seen_quotes.add(x['quote'])		
-with open('data_unique/unique_quotes.json', 'w') as outfile:
-    outfile.write(u'{ \"data\": [')
-    for i, _ in enumerate(unique_quotes):
-        if(i != 0):
-            outfile.write(u',')
 
-        json.dump(unique_quotes[i], outfile, ensure_ascii=False, indent=4)
-    outfile.write(u']}')
+with open('data_unique/unique_quotes.json', 'w') as unique_quotes_file:
+    json.dump({'data': list(unique_quotes)}, unique_quotes_file, indent=4)
+    unique_quotes_file.write('\n')
