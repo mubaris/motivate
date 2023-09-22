86e26bd56ecd754af16f1c41cfa46807192fa9e1
Neesara <neethums333@gmail.com>
2023-09-22 23:27:00 +0530

    Update motivate_gui.py
    Commented option to display timesheet reminder

--- motivate_gui.py
+++ motivate_gui.py
@@ -10,8 +10,8 @@ command=os.getcwd()+'/motivate/motivate.py'
 quote=os.popen(command).read()
 color=random.choice(['green','blue','purple','red','orange','brown','magenta','violet','maroon','olive','lime','teal','navy','DarkSlateGray','m','indigo','crimson'])
 label=tk.Label(root, text = quote ,fg=color, bg='white', font='Helvetica 10',wraplength=900).pack()
-if datetime.datetime.today().weekday() == 4 :
-    label=tk.Label(root, text = "Timesheet!!" ,fg='red', bg='white', font='Helvetica 20',wraplength=900,pady=10).pack()
+#if datetime.datetime.today().weekday() == 4 :
+#    label=tk.Label(root, text = "Timesheet!!" ,fg='red', bg='white', font='Helvetica 20',wraplength=900,pady=10).pack()
 quit_btn=tk.Button(root,text="Quit",command=root.destroy)
 quit_btn.pack(side="bottom")
 root.mainloop()
